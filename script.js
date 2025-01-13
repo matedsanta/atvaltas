816cd2f3f95efeff455a87fbe7f1b5de14a6f164
matedsanta <santa.mate.daniel@gmail.com>
2025-01-13 12:07:41 +0100

    Lemaradt script

--- script.js
+++ script.js
@@ -1,89 +1,149 @@
 const DecimalToBinary = (num) => {
-    let binary_number = "";
+    let binarisSzam = "";
 
     while (num !== 0) {
-        binary_number = (num % 2).toString() + binary_number;
+        binarisSzam = (num % 2).toString() + binarisSzam;
         num = Math.floor(num / 2);
     }
 
-    return binary_number || "0"; 
+    return binarisSzam || "0";
 };
+
 const BinaryToDecimal = (num) => {
-    let decimal_number = 0;
-    let power = 0;
+    let decimalisSzam = 0;
+    let hatvany = 0;
 
     while (num > 0) {
-        const last_digit = num % 10;
-        decimal_number += last_digit * Math.pow(2, power);
+        const utolsoSzamjegy = num % 10;
+        decimalisSzam += utolsoSzamjegy * Math.pow(2, hatvany);
         num = Math.floor(num / 10);
-        power++;
+        hatvany++;
     }
 
-    return decimal_number;
-};
-
-const OctalToDecimal = (num) => {
-    let decimal_number = 0;
-    let power = 0;
-
-    while (num > 0) {
-        const last_digit = num % 10;
-        decimal_number += last_digit * Math.pow(8, power);
-        num = Math.floor(num / 10);
-        power++;
-    }
-
-    return decimal_number;
+    return decimalisSzam;
 };
 
 const DecimalToOctal = (num) => {
-    let octal_number = "";
+    let oktalisSzam = "";
 
     while (num !== 0) {
-        octal_number = (num % 8).toString() + octal_number;
+        oktalisSzam = (num % 8).toString() + oktalisSzam;
         num = Math.floor(num / 8);
     }
 
-    return octal_number || "0";
+    return oktalisSzam || "0";
 };
 
-const HexadecimalToDecimal = (num) => {
-    let decimal_number = 0;
-    let power = 0;
-    num = num.toUpperCase(); // Ensure all characters are uppercase for consistency
+const OctalToDecimal = (num) => {
+    let decimalisSzam = 0;
+    let hatvany = 0;
 
-    while (num.length > 0) {
-        const last_char = num[num.length - 1];
-        let last_digit;
-
-        // Check if the character is a digit or a letter (A-F)
-        if (last_char >= "0" && last_char <= "9") {
-            last_digit = parseInt(last_char, 10);
-        } else if (last_char >= "A" && last_char <= "F") {
-            last_digit = last_char.charCodeAt(0) - 55; // A=10, B=11, ..., F=15
-        }
-
-        decimal_number += last_digit * Math.pow(16, power);
-        num = num.slice(0, -1); // Remove the last character
-        power++;
+    while (num > 0) {
+        const utolsoSzamjegy = num % 10;
+        decimalisSzam += utolsoSzamjegy * Math.pow(8, hatvany);
+        num = Math.floor(num / 10);
+        hatvany++;
     }
 
-    return decimal_number;
+    return decimalisSzam;
 };
 
 const DecimalToHexadecimal = (num) => {
-    let hexadecimal_number = "";
+    let hexadecSzam = "";
 
     while (num !== 0) {
-        const last_digit = num % 16;
-        hexadecimal_number =
-            (last_digit < 10
-                ? last_digit
-                : String.fromCharCode(last_digit + 55)
-            ).toString() + hexadecimal_number;
+        const utolsoSzamjegy = num % 16;
+        hexadecSzam =
+            (utolsoSzamjegy < 10
+                ? utolsoSzamjegy
+                : String.fromCharCode(utolsoSzamjegy + 55)) +
+            hexadecSzam;
         num = Math.floor(num / 16);
     }
 
-    return hexadecimal_number || "0"; // If the number is 0, return "0"
+    return hexadecSzam || "0";
 };
 
+const HexadecimalToDecimal = (num) => {
+    let decimalisSzam = 0;
+    let hatvany = 0;
+
+    num = num.toUpperCase(); // Nagybetűssé alakítás
+
+    while (num.length > 0) {
+        const utolsoKarakter = num[num.length - 1];
+        let utolsoSzamjegy;
+
+        if (utolsoKarakter >= "0" && utolsoKarakter <= "9") {
+            utolsoSzamjegy = parseInt(utolsoKarakter, 10);
+        } else if (utolsoKarakter >= "A" && utolsoKarakter <= "F") {
+            utolsoSzamjegy = utolsoKarakter.charCodeAt(0) - 55; // A=10, B=11, ..., F=15
+        } else {
+            alert("Érvénytelen hexadecimális szám!");
+            return null;
+        }
+
+        decimalisSzam += utolsoSzamjegy * Math.pow(16, hatvany);
+        num = num.slice(0, -1); // Az utolsó karakter eltávolítása
+        hatvany++;
+    }
+
+    return decimalisSzam;
+};
+
+valtasGomb.addEventListener("click", () => {
+    const bemenetiElem = document.querySelector(`datalist option[value="${eredeti.value}"]`);
+    const kimenetiElem = document.querySelector(`datalist option[value="${kimeneti.value}"]`);
+
+    if (bemenetiElem && kimenetiElem) {
+        const bemenetId = parseInt(bemenetiElem.dataset.id, 10);
+        const kimenetId = parseInt(kimenetiElem.dataset.id, 10);
+        let szam = beszam.value;
+
+        if (bemenetId === 1 || bemenetId === 4) {
+            szam = parseInt(szam, 10);
+        }
+
+        if (isNaN(szam) && bemenetId !== 2) {
+            alert("Érvénytelen szám!");
+            return;
+        }
+
+        switch (bemenetId) {
+            case 1: // Decimális
+                break;
+            case 2: // Hexadecimális
+                szam = HexadecimalToDecimal(szam);
+                break;
+            case 3: // Oktális
+                szam = OctalToDecimal(parseInt(szam, 10));
+                break;
+            case 4: // Bináris
+                szam = BinaryToDecimal(parseInt(szam, 10));
+                break;
+            default:
+                alert("Érvénytelen bemenet!");
+                return;
+        }
+
+        switch (kimenetId) {
+            case 1: // Decimális
+                kiszam.value = szam;
+                break;
+            case 2: // Hexadecimális
+                kiszam.value = DecimalToHexadecimal(szam);
+                break;
+            case 3: // Oktális
+                kiszam.value = DecimalToOctal(szam);
+                break;
+            case 4: // Bináris
+                kiszam.value = DecimalToBinary(szam);
+                break;
+            default:
+                alert("Érvénytelen kimenet!");
+                return;
+        }
+    } else {
+        alert("Érvénytelen választás!");
+    }
+});
